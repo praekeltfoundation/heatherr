376e811560e09ef28271ca13ab44bd262ca3a1c5
nathanbegbie <nathanbegbie@gmail.com>
2015-06-23 22:50:11 +0200

    created basic schema

--- slackhook/announce/models.py
+++ slackhook/announce/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.group_name
+
+
+class Person(models.Model):
+    person_name = models.CharField(max_length=200)
+    groups = models.ManyToManyField(Group)
+
+    def __unicode__(self):
+        return self.person_name
