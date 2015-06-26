f4eed4616d82864f4ee43c0332ab71d13de7bc65
nathanbegbie <nathanbegbie@gmail.com>
2015-06-26 14:17:31 +0200

    Split view logic into class Bellman

---
+++ slackhook/announce/bellman.py
@@ -0,0 +1,97 @@
+from .models import Group, Person
+from django.views.decorators.csrf import csrf_exempt
+
+
+class Bellman:
+
+    def __init__(self, text, user_name, user_id):
+        temp_command, space, self.text = text.partition(' ')
+        self.command = temp_command.lower()
+        self.user_name = user_name
+        self.user_id = user_id
+        self.response_text = ''
+
+    # list groups
+    def list_groups(self):
+        pass
+
+    # people-in-group
+    def people_in_group(self):
+        pass
+
+    # list-my-groups
+    def list_my_groups(self):
+        pass
+
+    # opt-in
+    def opt_in(self):
+        pass
+
+    # opt-out
+    def opt_out(self):
+        pass
+
+    # announce
+    def announce(self):
+        pass
+
+    # help - autoresponse
+    def help(self):
+        pass
+
+    # HELPER FUNCTIONS
+
+    # execute (works out what function to run, based on command)
+    def execute(self):
+        if (self.command == 'list-groups' or
+                self.command == 'list_groups'):
+            self.list_groups()
+        elif (self.command == 'people-in-group' or
+                self.command == 'people_in_group'):
+            self.people_in_group()
+        elif (self.command == 'list-my-groups' or
+                self.command == 'list-my-group' or
+                self.command == 'list_my_groups' or
+                self.command == 'list_my_group'):
+            self.list_my_groups()
+        elif (self.command == 'opt-in' or
+                self.command == 'opt_in'):
+            self.opt_in()
+        elif (self.command == 'opt-out' or
+                self.command == 'opt_out'):
+            self.opt_out()
+        elif self.command == 'announce':
+            self.announce()
+        elif self.command == 'help':
+            self.help()
+        else:
+            self.response_text = 'Sorry, I didn\'t',
+            'understand your command of: \'', self.command, '\'\n\n'
+            self.help()
+
+    def group_exists(self, group_name):
+        print 'running group_exists'
+        print 'Existing Groups:'
+        for group in Group.objects.all():
+            print group
+        test_group = Group(group_name=group_name)
+        if test_group in Group.objects.all():
+            print "group exists!"
+        else:
+            print "group does not exist"
+        return test_group in Group.objects.all()
+
+    def person_exists(self, user_id):
+        return Person.object(person_id=user_id) in Person.objects.all()
+
+    def name_changed(self, user_id, user_name):
+        return user_name != Person.objects.get(person_id=user_id).person_name
+
+    @csrf_exempt
+    def make_group(self, group_name):
+        print 'running make_group'
+        g = Group(group_name=group_name)
+        g.save()
+
+    def message_group_created(self, user_name, group_name):
+        return 'Thanks ', user_name, ' you created the group: ', group_name
--- slackhook/announce/views.py
+++ slackhook/announce/views.py
@@ -2,45 +2,12 @@ from django.http import HttpResponse  # JsonResponse
 from .models import Group, Person
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from bellman import Bellman
 # Create your views here.
 
 
 def check_token_safe(token):
     return token == settings.SLACK_TOKEN
-
-
-def group_exists(group_name):
-    print 'running group_exists'
-    print 'Existing Groups:'
-    for group in Group.objects.all():
-        print group
-
-    test_group = Group(group_name=group_name)
-    if test_group in Group.objects.all():
-        print "group exists!"
-    else:
-        print "group does not exist"
-    return test_group in Group.objects.all()
-
-
-def person_exists(user_id):
-    return Person.object(person_id=user_id) in Person.objects.all()
-
-
-def name_changed(user_id, user_name):
-    return user_name != Person.objects.get(person_id=user_id).person_name
-
-
-@csrf_exempt
-def make_group(group_name):
-    print 'running make_group'
-    g = Group(group_name=group_name)
-    g.save()
-
-
-def message_group_created(user_name, group_name):
-    return 'Thanks ', user_name, ' you created the group: ', group_name
-
 
 def update_user_info(user_id, user_name):
     p = Person(person_id=user_id, person_name=user_name)
@@ -58,7 +25,11 @@ def announce(request):
     print "---------------------------------------"
     # default empty text field will be ignored by slack
     response_text = ''
+
+    # security check
     if request.method == 'POST' and check_token_safe(request.POST['token']):
+        
+        # Checker statements (to be deleted)
         print 'POST from northhq slack'
         print request.POST
         user_name = request.POST['user_name']
@@ -66,50 +37,14 @@ def announce(request):
         print 'args:', args
         num_of_args = len(args)
         print 'num_of_args:', num_of_args
-        if num_of_args > 0:
-            # CREATE GROUP
-            if args[0] == 'create':
-                # is there an argument (i.e. a group name)
-                if num_of_args > 1:
-                    # Does the group already exist
-                    if group_exists(args[1]):
-                        response_text = "That group already exists"
-                    else:
-                        make_group(args[1])
-                        response_text = message_group_created(
-                            user_name, args[1])
-                else:
-                    response_text = 'Hey there NAME, you haven\'t given me a',
-                    'group name to create'
 
-            # LIST GROUPS
-            elif args[0] == 'list-groups':
-                response_text = 'list of groups: \n'
-                for group in Group.objects.all():
-                    response_text += (str(group) + '\n')
+        #check that POST has the correct form?
 
-            # OPT-IN
-            elif args[0] == 'opt-in':
-                if num_of_args > 1:
-                    # does the group exist?
-                    if group_exists(args[1]):
-                        # check if user exists and has the same user_name
-                        update_user_info(
-                            request.POST['user_id'], request.POST['user_name'])
-                        person = Person.objects.get(
-                            person_id=request.POST['user_id'])
-                        group = Group.objects.get(group_name=args[1])
+        bellman = Bellman(
+            text=request.POST['text'], 
+            user_name=request.POST['user_name'], 
+            user_id=request.POST['user_id'])
 
-                        group.save()
-                        person.groups.add(group)
-                        person.save()
-                else:
-                    response_text = 'Hey there NAME, you haven\'t given me a',
-                    'group name to opt-in to'
-        else:
-            response_text = 'Nothing has been accomplished'
-    else:
-        print "It's not legitimate"
-    print response_text
-    print "---------------------------------------"
+        bellman.
+
     return HttpResponse(response_text)
