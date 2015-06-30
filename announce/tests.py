4f7db1ae1b77103434740f70870f6dba3dbc28d7
nathanbegbie <nathanbegbie@gmail.com>
2015-06-30 12:46:12 +0200

    Created two passing tests

--- announce/tests.py
+++ announce/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client, override_settings
+from announce.models import Group, Person
 
-# Create your tests here.
+
+def make_post(user_name='bob', user_id='test_id', token='1234abc', text=''):
+    return {'token': token,
+            'user_name': user_name,
+            'user_id': user_id,
+            'text': text,
+            }
+
+
+@override_settings(SLACK_TOKEN='1234abc')
+class AnnounceTestCase(TestCase):
+    def setUp(self):
+        Group.objects.create(group_name='test_group')
+        Person.objects.create(person_name='bob', person_id='test_id')
+
+    def test_group_listing(self):
+        c = Client()
+        response = c.post('/announce/', make_post(text='list-groups'))
+        self.assertTrue('test_group' in response.content)
+        self.assertEqual(1, 1)
+        print response.content
+
+    def test_groups(self):
+        g = Group.objects.get(group_name='test_group')
+        self.assertFalse(g.person_set.all())
