9adc945efab84611fd110a1670a4ec82595fee6a
Simon de Haan <simon@praekeltfoundation.org>
2015-07-08 07:55:04 +0200

    fix tests and decorator

--- announce/views.py
+++ announce/views.py
@@ -11,11 +11,13 @@ from bellman import Bellman
 
 
 @csrf_exempt
-def require_slack_token(func):
-    def decorator(slack_token):
+def require_slack_token(slack_token):
+    def decorator(func):
         def handler(request, *args, **kwargs):
             if slack_token == request.POST.get('token'):
                 return func(request, *args, **kwargs)
+            return HttpResponseBadRequest('Invalid or missing slack token.')
+        return handler
     return decorator
 
 
@@ -24,31 +26,21 @@ def announce(request):
     logger.debug("---------------------------------------")
     # default empty text field will be ignored by slack
 
-    # security check
-    if request.method == 'POST' and check_token_safe(request.POST['token']):
+    logger.debug('POST from northhq slack')
+    logger.debug(request.POST)
+    args = request.POST['text'].split()
+    logger.debug('args: %r' % (args,))
+    num_of_args = len(args)
+    logger.debug('num_of_args: %r' % (num_of_args,))
+    logger.debug("---------------------------------------")
 
-        logger.debug('POST from northhq slack')
-        logger.debug(request.POST)
-        args = request.POST['text'].split()
-        logger.debug('args: %r' % (args,))
-        num_of_args = len(args)
-        logger.debug('num_of_args: %r' % (num_of_args,))
-        logger.debug("---------------------------------------")
+    # check that POST has the correct form?
 
-        # check that POST has the correct form?
+    app = Bellman(
+        text=request.POST['text'],
+        user_name=request.POST['user_name'],
+        user_id=request.POST['user_id'])
 
-        app = Bellman(
-            text=request.POST['text'],
-            user_name=request.POST['user_name'],
-            user_id=request.POST['user_id'])
+    app.execute()
 
-        app.execute()
-
-        return HttpResponse(app.get_response())
-
-    elif request.method != 'POST':
-        return HttpResponseBadRequest('Did not receive an HTTP POST.')
-    elif not check_token_safe(request.POST['token']):
-        return HttpResponseBadRequest('Received an invalid token.')
-    else:
-        HttpResponseBadRequest('Not sure how to handle this request.')
+    return HttpResponse(app.get_response())
