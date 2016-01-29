6fcf5dc94ebada6045fbbe8e6ad783ca28705082
Simon de Haan <simon@praekeltfoundation.org>
2016-01-30 00:04:53 +0100

    request user:read scope

---
+++ heatherr/timezone/commands.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+from heatherr.models import SlackAccount
+from heatherr.views import dispatcher
+
+
+timezone = dispatcher.command('/time')
+
+@timezone.respond('r^for (.+)$')
+def for_(request, match):
+    print match
