5786bb85f8969cea6b9b0c9ab2fe769c7968b47e
Simon de Haan <simon@praekeltfoundation.org>
2016-02-03 08:53:39 +0100

    wip

--- heatherr/__init__.py
+++ heatherr/__init__.py
@@ -1,4 +1,5 @@
 from .views import dispatcher
+from .celery import app as celery_app
 
 
-__all__ = ['dispatcher']
+__all__ = ['dispatcher', 'celery_app']
--- heatherr/checkin/models.py
+++ heatherr/checkin/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+import arrow
+
 
 class Checkin(models.Model):
 
@@ -16,3 +18,21 @@ class Checkin(models.Model):
     last_checkin = models.DateTimeField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, auto_now_add=True)
     updated_at = models.DateTimeField(blank=True, auto_now=True)
+
+    def get_user_info(self):
+        data = self.slackaccount.api_call('users.info', user=self.user_id)
+        return data['user']
+
+    def needs_checkin(self, interval, current_time=None):
+        current_time = arrow.get(current_time or arrow.utcnow())
+
+        days = {
+            Checkin.DAILY: 1,
+            Checkin.WEEKLY: 7,
+        }[interval]
+
+        yesterday = current_time - timedelta(days=1)
+        local_time = current_time.to(user_info['tz'])
+        return (
+            ((self.last_checkin - yesterday).hours) > 24 and
+            (8 < local_time.hour < 9))
