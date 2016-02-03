0cb6c48231ed3ce495de8e55fe938098cb836b67
Simon de Haan <simon@praekeltfoundation.org>
2016-02-03 20:51:53 +0100

    add more scopes

--- heatherr/settings.py
+++ heatherr/settings.py
@@ -126,6 +126,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN', '')
 SLACK_CLIENT_ID = os.environ.get('SLACK_CLIENT_ID', '')
 SLACK_CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET', '')
+SLACK_SCOPES = 'incoming-webhook,commands,bot,users:read,im:read,im:write'
 
 LOGGING = {
     'version': 1,
