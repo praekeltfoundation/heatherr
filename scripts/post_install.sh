66f83707c9224f61b07fa51215f8795e234a1593
Simon de Haan <simon@praekeltfoundation.org>
2016-02-11 21:13:42 +0100

    not needed, handled by sideloader

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,8 +2,3 @@ pip install -e ${INSTALLDIR}/${NAME}/
 
 django-admin.py migrate --noinput --settings=heatherr.settings
 django-admin.py collectstatic --noinput --settings=heatherr.settings
-
-supervisorctl update
-supervisorctl restart heatherr
-supervisorctl restart heatherr_celery
-supervisorctl restart heatherr_relay
