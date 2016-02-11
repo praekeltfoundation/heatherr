6c4948d7eb3701d8bd983d06a9517646c2040f96
Simon de Haan <simon@praekeltfoundation.org>
2016-02-11 21:13:13 +0100

    supervisorctl restarts on post-install

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -2,3 +2,8 @@ pip install -e ${INSTALLDIR}/${NAME}/
 
 django-admin.py migrate --noinput --settings=heatherr.settings
 django-admin.py collectstatic --noinput --settings=heatherr.settings
+
+supervisorctl update
+supervisorctl restart heatherr
+supervisorctl restart heatherr_celery
+supervisorctl restart heatherr_relay
