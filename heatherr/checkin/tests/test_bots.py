5212c9ef8b1b84bcdd25d2a9e006c44e12cd4ec1
Simon de Haan <simon@praekeltfoundation.org>
2016-02-17 00:27:49 +0100

    extra test

--- heatherr/checkin/tests/test_bots.py
+++ heatherr/checkin/tests/test_bots.py
@@ -1,6 +1,8 @@
 import re
+import json
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 from heatherr.checkin.bots import thankyou
 from heatherr.views import BotMessage
@@ -20,3 +22,17 @@ class TestBots(TestCase):
                 'id': None,
                 'channel': None
             })
+
+    def test_raw_thank_you(self):
+        resp = self.client.post(reverse('bots'), data=json.dumps({
+            u'text': u'thanks smn',
+            u'ts': u'1455664606.000022',
+            u'user': u'user-id',
+            u'team': u'team-id',
+            u'type': u'message',
+            u'channel': u'C1000',
+        }), content_type='application/json',
+            HTTP_X_BOT_USER_ID='bot-user-id',
+            HTTP_X_BOT_USER_NAME='bot-user-name')
+        [reply] = resp.json()
+        self.assertEqual(reply['text'], 'you thanked: smn')
