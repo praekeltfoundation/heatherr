0e527e2ab41dbf00dfeeabca7361d96cb22ccb42
Simon de Haan <simon@praekeltfoundation.org>
2016-02-19 20:29:26 +0100

    polls

--- heatherr/random/commands.py
+++ heatherr/random/commands.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import re
+
 from heatherr.views import dispatcher
 from heatherr.models import SlackAccount
 
@@ -25,3 +27,53 @@ def slap(request, match):
             user_id, target),
         as_user=True,
         channel=channel_id,)
+
+poll = dispatcher.command('/poll')
+
+
+@poll.respond(r'^(?P<question>.+)\? (?P<options>.+)$')
+def poll(request, match):
+    """
+    `/poll Should we jump off a bridge? Yes, No, Maybe?`
+
+    Create a poll
+    """
+    slackaccount = SlackAccount.objects.get(
+        team_id=request.POST['team_id'])
+    user_id = request.POST['user_id']
+    channel_id = request.POST['channel_id']
+    (question, option_str) = match.groups()
+    options = re.split(r',\s*|\sor\s+', option_str)[:10]
+
+    emoji_map = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'keycap_ten',
+    ]
+
+    post = ['Poll from <@%s>:' % (user_id,)]
+    post.append('%s?' % (question,))
+    post.extend([':%s: %s' % (emoji_map[idx], option)
+                 for (idx, option) in enumerate(options)])
+    print '\n'.join(post)
+
+    resp = slackaccount.api_call(
+        'chat.postMessage',
+        text='\n'.join(post),
+        channel=channel_id,
+        as_user=True,
+    )
+
+    for idx, option in enumerate(options):
+        slackaccount.api_call(
+            'reactions.add',
+            name=emoji_map[idx],
+            channel=channel_id,
+            timestamp=str(resp['ts']),)
--- heatherr/random/tests/test_commands.py
+++ heatherr/random/tests/test_commands.py
@@ -1,4 +1,3 @@
-import json
 from urllib import unquote_plus, unquote
 
 from heatherr.tests.base import CommandTestCase
@@ -14,10 +13,32 @@ class RandomTest(CommandTestCase):
     @responses.activate
     def test_slap(self):
 
-        self.mock_api_call('chat.postMessage', data=json.dumps({}))
+        self.mock_api_call('chat.postMessage', data={})
         self.send_command('/slap @foo')
         [call] = responses.calls
         args = unquote(unquote_plus(call.request.body))
         self.assertTrue(
             '<@user_id> slaps <@foo> around a bit with a large trout.'
             in args)
+
+    @responses.activate
+    def test_poll(self):
+        self.mock_api_call('chat.postMessage', data={
+            'ts': 1,
+        })
+        self.mock_api_call('reactions.add', data={
+            'ok': True,
+        })
+        self.send_command(
+            '/poll Should we jump off a bridge? Yes, No or Maybe?')
+        [post, reaction1, reaction2, reaction3] = responses.calls
+        self.assertTrue('bridge' in post.request.body)
+        self.assertTrue('Yes' in post.request.body)
+        self.assertTrue('No' in post.request.body)
+
+        self.assertTrue(',' not in post.request.body)
+        self.assertTrue('or' not in post.request.body)
+
+        self.assertTrue('name=one' in reaction1.request.body)
+        self.assertTrue('name=two' in reaction2.request.body)
+        self.assertTrue('name=three' in reaction3.request.body)
